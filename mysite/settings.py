c2616faa856cdf0a4434ead5a651e5d0d52d8e21
ishwar ambare <ishuambare@gmail.com>
2020-11-24 12:53:50 +0530

    adding jinja settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -53,6 +53,16 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
+
+    {
+        'BACKEND': 'django.template.backends.jinja2.Jinja2',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'environment': 'mysite.jinja2.environment'
+        },
+    },
+
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')]
