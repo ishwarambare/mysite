44ef042f39a9ae903f0ab9bf1d03b53c1d7f64df
ishwar ambare <ishuambare@gmail.com>
2020-11-25 17:27:16 +0530

    adding tag fun.

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,29 @@
 from django.core.mail import send_mail
 from django.http import HttpResponse
-
 from .forms import EmailPostForm, CommentForm
 from .models import *
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, \
-    PageNotAnInteger
 from django.views.generic import ListView
+from taggit.models import Tag
+from django.db.models import Count
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
+from django.views.generic import ListView
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
+from taggit.models import Tag
 
 
-def post_list(request):
+def post_list(request,tag_slug=None):
     object_list = Post.published.all()
-    paginator = Paginator(object_list, 2)
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
@@ -19,7 +31,7 @@ def post_list(request):
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html.j2', {'page': page, 'posts': posts})
+    return render(request, 'blog/post/list.html.j2', {'page': page, 'posts': posts,'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
