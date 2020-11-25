7ff9c5654414657eba35f2e5e04a50f645b1050d
ishwar ambare <ishuambare@gmail.com>
2020-11-25 09:14:50 +0530

    post detail view with error solve

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse, reverse_lazy
 from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
 
-'''
+
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(status='published')
@@ -38,7 +38,10 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse_lazy('blog:post_detail', args=[self.id])
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day, self.slug])
 
 
 class Comment(models.Model):
@@ -58,52 +61,3 @@ class Comment(models.Model):
     def __str__(self):
         return f'Comment by {self.name} on {self.post}'
 
-
-'''
-from django.db import models
-from django.utils import timezone
-from django.urls import reverse
-from django.contrib.auth.models import User
-
-
-class PublishedManager(models.Manager):
-    def get_queryset(self):
-        return super().get_queryset().filter(status='published')
-
-
-class Post(models.Model):
-    STATUS_CHOICES = (
-        ('draft', 'Draft'),
-        ('published', 'Published'),
-    )
-    title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250,
-                            unique_for_date='publish')
-    author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               related_name='blog_posts')
-    body = models.TextField()
-    publish = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10,
-                              choices=STATUS_CHOICES,
-                              default='draft')
-
-    objects = models.Manager()  # The default manager.
-    published = PublishedManager()  # Our custom manager.
-
-    class Meta:
-        ordering = ('-publish',)
-
-    def __str__(self):
-        return self.title
-
-    # def get_absolute_url(self):
-    #     return reverse('blog:post_detail',
-    #                    args=[self.publish.year,
-    #                          self.publish.month,
-    #                          self.publish.day, self.slug])
-
-    def get_absolute_url(self):
-        return reverse('blog:post_detail', args=[self.id])
--- blog/urls.py
+++ blog/urls.py
@@ -13,9 +13,8 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    # post views
     # path('', views.post_list, name='post_list'),
     path('', views.PostListView.as_view(), name='post_list'),
-    # path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
-    path('<int:id>/', views.post_detail, name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    # path('<int:id>/', views.post_detail, name='post_detail'),
 ]
